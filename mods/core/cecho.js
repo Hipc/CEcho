5d69f4f9546ef0adb7a2d38f9c753264df91b41f
Hipc <hipc@icloud.com>
2016-08-04 23:37:33 +0800

    refact core
    Signed-off-by: Hipc <hipc@icloud.com>

--- mods/core/cecho.js
+++ mods/core/cecho.js
@@ -1,46 +1,34 @@
 /**
- * Created by HIPC on 16/7/25.
+ * Created by HIPC on 16/8/4.
  */
-
 $(document).ready(function () {
-    core.init();
+    var ceditor = coreEditor();
+    document.getElementsByTagName('body')[0].appendChild(ceditor);
 });
 
-var core = {
-    init:function () {
-        var textshow = document.createElement('div');
-        var textarea = document.createElement('textarea');
-        var cursorLayer = document.createElement('div');
-        var cursor = document.createElement('div');
-        textshow.setAttribute('class','core-textshow');
-        textshow.setAttribute('id','core-textshow-1');
-        textarea.setAttribute('class','core-textarea');
-        textarea.setAttribute('id','core-textarea-1');
-        // textarea.setAttribute('disabled','true');
-        cursorLayer.setAttribute('class','core-cursorlayer');
-        cursorLayer.setAttribute('id','core-cursorLayer-1');
-        cursor.setAttribute('class','core-cursor');
-        cursor.setAttribute('id','core-cursor-1');
-        var body = document.getElementsByTagName('body')[0];
-        body.appendChild(textarea);
-        body.appendChild(cursorLayer);
-        body.appendChild(cursor);
-        body.appendChild(textshow);
-
-
-        var coreInitComplete = new CustomEvent(
-            'core-init-complete'
-        );
-        window.dispatchEvent(coreInitComplete);
-    },
-    CORE_EVENT:{
-    },
-    getElememt:function () {
-        return {
-            textarea: $("#core-textarea-1"),
-            textshow: $("#core-textshow-1"),
-            cursor: $("#core-cursor-1"),
-            cursorLayer: $("#core-cursorLayer-1")
+var coreEditor = function(cssOptions){
+    var ceditor = document.createElement('div');
+    ceditor.setAttribute('class','core-ceditor');
+    ceditor.setAttribute('contenteditable','true');
+    var addBlankDiv = function () {
+        var firstLine = document.createElement('div');
+        firstLine.innerHTML = '<br>';
+        ceditor.appendChild(firstLine);
+    };
+    $(ceditor).on('focus',function () {
+        if($(this).text().trim() === '' && ($(this).html().length === 0)){
+            addBlankDiv();
         }
+    });
+    $(ceditor).on('input',function(){
+        if($(this).text().trim() === '' && ($(this).html().length === 0)){
+            addBlankDiv();
+        }
+    });
+    //reset css from given option's
+    for(var name in cssOptions){
+        ceditor.style[name] = cssOptions[name];
     }
+    return ceditor;
 };
+
